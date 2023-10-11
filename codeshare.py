b5e757e83a486dc5a3224fdacfd48daf6c824613
Amit Kumar <ak.nitrr13@gmail.com>
2023-10-11 19:42:56 +0530

    Update codeshare.py

--- codeshare.py
+++ codeshare.py
@@ -1,35 +1,31 @@
-# Import the OutputParser class
-from langchain.agents.conversational.output_parser import OutputParser
+from langchain.prompts import PromptTemplate
+from langchain.llms import OpenAI
+from langchain.output_parsers import PydanticOutputParser
+from pydantic import BaseModel, Field
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.vectorstores import Chroma
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.chains import ConversationalRetrievalChain
 
-# Define a subclass of OutputParser that parses the output into a list of answers
-class ListOutputParser(OutputParser):
-    def parse(self, output: str) -> list:
-        # Split the output by newlines and remove empty lines
-        lines = [line.strip() for line in output.split("\n") if line.strip()]
-        # Return the list of answers
-        return lines
+# Define your desired data structure.
+class MyData(BaseModel):
+    field1: str = Field(description="Description of field1")
+    field2: str = Field(description="Description of field2")
 
-    def get_format_instructions(self) -> str:
-        # Return a custom format instruction that tells the language model to return one answer per line
-        return "Please return one answer per line.\n"
-
-# Create an instance of the ListOutputParser
-output_parser = ListOutputParser()
-
-# Create a ConversationalRetrieval chain and pass the output parser as an option
-chain = ConversationalRetrievalQAChain.fromLLM(
-    model,
-    vectorstore.asRetriever(15),
-    {
-        "qaTemplate": QA_PROMPT,
-        "questionGeneratorTemplate": CONDENSE_PROMPT,
-        "verbose": True,
-        "outputParser": output_parser, # Pass the output parser instance
-    },
+# Set up a parser + inject instructions into the prompt template.
+parser = PydanticOutputParser(pydantic_object=MyData)
+prompt = PromptTemplate(
+    template="Answer the user query.\\n {format_instructions}\\n {query}\\n",
+    input_variables=["query"],
+    partial_variables={"format_instructions": parser.get_format_instructions()}
 )
 
-# Call the chain and use the output parser to parse the response
-question = "What are some benefits of meditation?"
-response = chain.call({"question": question})
-answers = output_parser.parse(response)
-print(answers)
+# Initialize ConversationalRetrievalChain
+embeddings = OpenAIEmbeddings()
+vectorstore = Chroma.from_documents(documents, embeddings)
+qa_chain = ConversationalRetrievalChain.from_llm(OpenAI(temperature=0), vectorstore.as_retriever())
+
+# Use ConversationalRetrievalChain and parser
+_input = prompt.format_prompt(query=my_query)
+output = qa_chain(_input.to_string())
+parsed_output = parser.parse(output)
